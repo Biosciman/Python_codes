c81b1892a47202e905e3592b6a9ded9fa0ae7e86
Biosciman <lmzhang1994@gmail.com>
2020-08-08 17:58:04 +0800

    Numpay training

---
+++ Numpy training.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# 用 numpy 创建一个 2 * 2 的二维数组 ndarray，指定元素类型为 float，命名为 arr1
+arr1 = np.array([[2]*2]*2, dtype=float)
+
+
+# 生成元素全为 0 的 6 * 6 矩阵 arr2；元素全为 1 的 6 * 6 矩阵 arr3；以及 6 * 6 的单位矩阵 arr4
+arr2 = np.zeros((6, 6))
+arr3 = np.ones((6, 6))
+
+print(arr4)
