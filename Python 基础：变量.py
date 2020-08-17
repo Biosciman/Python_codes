a49f3069872ec4486f3595bca3c90eb96a6a5b51
Biosciman <lmzhang1994@gmail.com>
2020-08-17 23:27:02 +0800

    Python 基础：变量.py

---
+++ Python 基础：变量.py
@@ -0,0 +1,17 @@
+# 输出变量
+# 让用户猜一个数字，将用户输入的数字打印出来
+Number = input('请你输入一个数字：')
+print('你输入的数字是：' + Number)
+
+# 变量计算
+# 将 money 变量的值乘以 2 再加上 50
+# 赋值给 result
+money = 20
+result = money * 2 + 50
+
+# 变量类型
+a = 'hello,world' # 输出：hello,world
+b = 1.414 # 输出：1.414
+c = (1 != 2) # 输出 True
+d = (True and False) # 输出：Flase
+e = (True or False) # 输出： True
