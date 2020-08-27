86da827fc5ce4a63f79ab995474f0af96edf190a
Biosciman <lmzhang1994@gmail.com>
2020-08-27 23:21:27 +0800

    利用数据透视表统计分析销售数据.py

--- 利用数据透视表统计分析销售数据.py
+++ 利用数据透视表统计分析销售数据.py
@@ -11,3 +11,14 @@ pd.pivot_table(Dataframe, index=['id', ], values=['sales'], columns=['date'],
                      aggfunc=np.sum,  margins=True, fill_value=0)
 
 # 以下为部分抽烟人群调查表，请将数据存储入 DataFrame，并完成以下操作
+# 利用 pandas 读取 task6_2.csv 文件中的数据，建立 DataFrame
+df = pd.read_csv('task6_2.csv')
+# 请以性别为分组依据，查看男女抽烟平均年龄和身高
+df['sex'] = df['sex'].astype('category')
+df['sex'].cat.set_categories(['man', 'women'], inplace=True)
+pd.pivot_table(df, index=['sex'], values=['age', 'height'], aggfunc=np.mean)
+# 利用交叉表知识，统计各个性别抽烟的人数
+pd.crosstab(index=[df['sex']], columns=[df['smoke']], margins=True)
+# 利用交叉表知识，统计各个年龄段抽烟人情况
+a = pd.crosstab(df.age, df.smoke)
+print(a)
