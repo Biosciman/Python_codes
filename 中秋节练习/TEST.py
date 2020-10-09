8c6e87c188d03c3a53cd110b76a1ff98798c3725
Biosciman <lmzhang1994@gmail.com>
2020-10-09 23:36:57 +0800

    爬虫（中秋节）

--- 中秋节练习/TEST.py
+++ 中秋节练习/TEST.py
@@ -1,21 +1,11 @@
 from selenium import webdriver
 import time
 
-# 创建浏览器对象，该操作会自动帮我们打开Google浏览器窗口
 browser = webdriver.Chrome()
-
-# 调用浏览器对象，向服务器发送请求。该操作会打开Google浏览器，并跳转到“百度”首页
-browser.get('https://www.baidu.com')
-
-# 刷新浏览器
-browser.refresh()
-
-# 最大化窗口
+browser.get('https://youhui.pinduoduo.com/')
+key_word = '电脑'
+getvalue = browser.find_element_by_class_name('search-bar-input')
+getvalue = getvalue.find_element_by_xpath('..//div/*')
+browser.execute_script('arguments[0].value = "电脑";', getvalue)
+browser.find_element_by_class_name('search-bar-input').click()
 browser.maximize_window()
-
-# 定位"抗击肺炎"链接内容
-element = browser.find_element_by_id('kw')
-element.send_keys('hhhh')
-element = browser.find_element_by_id('su')
-element.click()
-# browser.refresh()
--- 中秋节练习/搜索商品.py
+++ 中秋节练习/搜索商品.py
@@ -8,7 +8,7 @@ def search_product(key_word):
     # 定义点击按钮，并点击。淘宝网的网页源代码:
     # <div class="search-button">
     # <button class="btn-search tb-bg" type="submit" data-spm-click="gostr=/tbindex;locaid=d13">搜索</button></div>
-    browser.find_element_by_class_name('search-button').click()
+    browser.find_element_by_class_name('btn-search').click()
     # 最大化窗口
     browser.maximize_window()
     # 等待15秒
