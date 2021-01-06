273a8086ab9c39b513f14b3655aca3364523a387
Biosciman <lmzhang1994@gmail.com>
2021-01-06 22:20:06 +0800

    xml

--- 解析xml.py
+++ 解析xml.py
@@ -5,6 +5,7 @@ DOMTree = xml.dom.minidom.parse('Human_Chymotrypsin.xml')
 
 doc = DOMTree.documentElement
 
+
 for i in doc.childNodes[1].childNodes:
     if i.nodeName == 'Seq-entry':
         for j in i.childNodes:
@@ -13,4 +14,20 @@ for i in doc.childNodes[1].childNodes:
                     if k.nodeName == 'Bioseq':
                         for a in k.childNodes:
                             if a.nodeName == 'Bioseq_id':
-                                print(a.v)
+                                continue
+
+                            if a.nodeName == 'Bioseq_descr':
+                                continue
+
+                            if a.nodeName == 'Bioseq_inst':
+                                for b in a.childNodes:
+                                    if b.nodeName == 'Seq-inst':
+                                        for c in b.childNodes:
+                                            if c.nodeName == 'Seq-inst_repr':
+                                                print(c.getAttribute('value'))
+                                            if c.nodeName == 'Seq-inst_mol':
+                                                print(c.getAttribute('value'))
+                                            if c.nodeName == 'Seq-inst_length':
+                                                print(c.childNodes[0].data)
+                            if a.nodeName == 'Bioseq_annot':
+                                continue
