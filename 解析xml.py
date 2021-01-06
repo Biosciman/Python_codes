a059bf4c99fa9eedd7d92a7b030da48d85fec0c0
Biosciman <lmzhang1994@gmail.com>
2021-01-06 23:13:57 +0800

    xml

--- 解析xml.py
+++ 解析xml.py
@@ -1,10 +1,10 @@
 import xml.dom.minidom
 from xml.dom.minidom import parse
+import xml.etree.ElementTree
 
-DOMTree = xml.dom.minidom.parse('Human_Chymotrypsin.xml')
-
+# minidom解析xml
+DOMTree = xml.dom.minidom.parse(r'Human_Chymotrypsin.xml')
 doc = DOMTree.documentElement
-
 
 for i in doc.childNodes[1].childNodes:
     if i.nodeName == 'Seq-entry':
@@ -31,3 +31,17 @@ for i in doc.childNodes[1].childNodes:
                                                 print(c.childNodes[0].data)
                             if a.nodeName == 'Bioseq_annot':
                                 continue
+
+# etree解析xml
+root = xml.etree.ElementTree.parse(r'Human_Chymotrypsin.xml')
+nodes = root.getiterator() # 新版本用 nodes = root.iter() 或者 nodes = list(root.iter())
+for node in nodes:
+    if node.tag == 'Seq-inst_length':
+        print(node.tag, node.text)
+
+ele_findall = root.findall('Bioseq-set_seq-set')
+print(type(ele_findall))
+print(ele_findall[0].tag)
+
+for i in root.iter('Seq-inst_length'):
+    print(i.text)
